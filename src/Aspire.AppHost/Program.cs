bd06bcca51a9b3f695f39e81ea240637b8b15aa6
Stephen Newman <steve@newman.digital>
2024-05-15 18:42:05 +0100

    Add Blazor application to be the client management app.

--- src/Aspire.AppHost/Program.cs
+++ src/Aspire.AppHost/Program.cs
@@ -2,9 +2,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddContainer("graphdb", "neo4j", "5.19.0-community-bullseye")
     .WithEnvironment("NEO4J_AUTH", "none")
-    .WithEndpoint(7474, targetPort: 7474, name: "neo4j-http")
+    .WithEndpoint(7474, targetPort: 7474, scheme:"http", name: "neo4j-http")
     .WithEndpoint(7687, targetPort: 7687, name: "neo4j-bolt");
 
 builder.AddRabbitMQ("messaging");
+
+builder.AddProject<Projects.ClientManagement>("clientmanagement-web");
 
 builder.Build().Run();
